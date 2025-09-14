615287336e594780377c28897f69cf0e0b162732
jeremiedr <jeremiedrouinl@gmail.com>
2025-09-14 17:40:07 -0400

    Update game.js

--- game.js
+++ game.js
@@ -193,7 +193,7 @@ let level10 = [
   ' ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd            ',
 ];
 
-let levels = [level1, level2, level3, level4, level5, level6, level7, level8, level9, level10];
+let levels = [level4, level5, level6, level7, level8, level9, level10, level1, level2, level3];
 
 /* ============================= ASSETS & AUDIO ============================= */
 
